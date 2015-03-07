9f39a39931e2c6b6827eefade5d5d6cc2d799a2b
beepscore <support@beepscore.com>
2015-03-07 08:23:50 +0000

    Add 04_05_blink.js

---
+++ 04_05_blink.js
@@ -0,0 +1,17 @@
+// 04_05_blink.js
+
+var b = require('bonescript');
+
+var led = "USR3";
+b.pinMode(led, b.OUTPUT);
+
+var state = 0;
+
+function toggleLED() {
+    // ^ is bitwise xor
+    state = state ^ 1;
+    b.digitalWrite(led, state);
+}
+
+setInterval(toggleLED, 500);
+
