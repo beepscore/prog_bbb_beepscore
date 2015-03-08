74995baa1e36d4e5f1a49666534029f0276a5f15
beepscore <support@beepscore.com>
2015-03-08 17:36:12 +0000

    Add 05_array_enumerate.js

---
+++ 05_array_enumerate.js
@@ -0,0 +1,7 @@
+var people = ["fred", "mary", "becky", "steve"];
+
+for (var i = 0; i < people.length; i++) {
+    console.log("people[" + i + "] " + people[i]);
+}
+
+people.forEach(function(person) {console.log(person);});
