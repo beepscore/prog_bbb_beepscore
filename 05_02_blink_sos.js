98b150918fde62336dc1db4c77b9e4e429ea8c0f
beepscore <support@beepscore.com>
2015-03-08 18:55:21 +0000

    In 05_02_blink_sos.js rename variables, re-indent, add comments

--- 05_02_blink_sos.js
+++ 05_02_blink_sos.js
@@ -2,34 +2,43 @@
 
 var b = require('bonescript');
 
+// durations in milliseconds
 var dot = 200;
 var dash = dot * 3;
 var gap = dot;
 
 var leds = ["USR0", "USR1", "USR2", "USR3"];
+// configure all leds as outputs
 leds.forEach(function(led) { b.pinMode(led, b.OUTPUT); });
 
-
-function ledsOn() { // turn the four on-board LEDs on
-    leds.forEach(function(led) { b.digitalWrite(led, 1); });
+function ledsOn() {
+  // turn the four on-board LEDs on
+  leds.forEach(function(led) {
+    b.digitalWrite(led, 1);
+  });
 }
-    
+
 function ledsOff() { 
-    leds.forEach(function(led) { b.digitalWrite(led, 0); });
+  leds.forEach(function(led) {
+    b.digitalWrite(led, 0);
+  });
 }
 
 function flash(period) {
-    ledsOn();
-    setTimeout(ledsOff, period);
+  ledsOn();
+  setTimeout(ledsOff, period);
 }
 
 function flashMessage(durations) {
-    var timeline = 0;
-    for (var i = 0; i < durations.length; i++) {
-        var d = durations[i];
-        setTimeout(function() { flash(d); }, timeline);
-        timeline = timeline + d + gap;
-    }
+  var timeOutMsec = 0;
+  for (var i = 0; i < durations.length; i++) {
+    var duration = durations[i];
+    // set a timer to fire at timeOutMsec
+    setTimeout(function() { flash(duration); }, timeOutMsec);
+    timeOutMsec = timeOutMsec + duration + gap;
+  }
 }
 
-flashMessage([ dot, dot, dot, dash, dash, dash, dot, dot, dot]);
+flashMessage([dot, dot, dot,
+    dash, dash, dash,
+    dot, dot, dot]);
